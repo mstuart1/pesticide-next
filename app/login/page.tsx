355e4d0420d1ea567fa3f610f763e29181b67a4a
Michelle Stuart <michelle.stuart@rutgers.edu>
2025-05-27 13:27:36 -0400

    Remove Header component from LoginPage and simplify layout structure

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,6 @@
 import LoginForm from '@/app/ui/login-form';
 import { Metadata } from 'next';
 import { Suspense } from 'react';
-import Header from '../ui/header';
-
 
 export const metadata: Metadata = {
   title: 'Login',
@@ -11,15 +9,13 @@ export const metadata: Metadata = {
 export default async function LoginPage() {
   
   return (
-   <main className="flex min-h-screen flex-col p-6">
-        
-          <Header/>
+   
       <div className="mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 ">
         
         <Suspense>
           <LoginForm />
         </Suspense>
       </div>
-    </main>
+   
   );
 }
