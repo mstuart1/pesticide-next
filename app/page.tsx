8b98d8bedef84c3110b45a55a62ae01eded4e1fd
Michelle Stuart <michelle.stuart@rutgers.edu>
2025-05-27 13:27:58 -0400

    Remove Header component from Page and adjust layout structure

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,7 @@ export default function Page() {
 
   return (
     
-    <main className="flex min-h-screen flex-col p-6">
-      <Header/>
+   
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
        
@@ -49,7 +48,7 @@ export default function Page() {
       />
         </div>
       </div>
-    </main>
+    
   
     
   );
