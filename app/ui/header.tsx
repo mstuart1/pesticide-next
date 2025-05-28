9fb0be7bc1dfc42a943e074fdaa7b34aa289066e
Michelle Stuart <michelle.stuart@rutgers.edu>
2025-05-28 12:13:50 -0400

    Refactor Header component layout to improve structure and include LogoutButton

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,10 +1,14 @@
 import DepLogo from "./dep-logo";
+import LogoutButton from "./logout-button";
 
 export default function Header() {
     return (
-        <div className="flex h-20 shrink-0 items-center justify-start rounded-lg bg-blue-900 p-4 md:h-52">
-                <DepLogo />
-                <p className='text-white md:text-5xl font-bold'>Pesticide Surveys</p>
-              </div>
+        <div className={`flex w-full justify-between items-center bg-blue-900 p-4`}>
+        <div className="flex h-20 shrink-0 items-center justify-start rounded-lg p-4 md:h-52">
+            <DepLogo />
+            <p className='text-white md:text-5xl font-bold'>Pesticide Surveys</p>
+        </div>
+            <LogoutButton />
+        </div>
     )
 }
